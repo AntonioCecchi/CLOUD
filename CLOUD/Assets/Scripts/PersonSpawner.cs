b4fe22b5f752c1fe78bfd5c4c9c7f8d8f8a451ee
AntoCecchi99 <antoniocecchi99@gmail.com>
2021-03-27 09:57:03 +0100

    Added spawner if player does not have children too long

---
+++ CLOUD/Assets/Scripts/PersonSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonSpawner : MonoBehaviour
+{
+    public GameObject person;
+
+    public GameObject leftSpawn;
+    public GameObject rightSpawn;
+
+    public GameObject player;
+
+    public float spawnTime;
+    public float spawnTimeMax;
+
+
+    void Start()
+    {
+        spawnTime = spawnTimeMax;
+    }
+
+    void Update()
+    {
+        if (player.GetComponent<Player_Physic>().totalChildrenNumber == 0)
+        {
+            spawnTime -= Time.deltaTime;
+        }
+
+        if (player.GetComponent<Player_Physic>().totalChildrenNumber > 0)
+        {
+            spawnTime = spawnTimeMax;
+        }
+
+        if(spawnTime <= 0)
+        {
+
+            spawnTime = spawnTimeMax;
+
+            SpawnPerson();
+        }
+
+    }
+
+    private void SpawnPerson()
+    {
+        float randomChance = UnityEngine.Random.Range(0.0f, 1.0f);
+
+        if (randomChance <= 0.5)
+        {
+            Instantiate(person, leftSpawn.transform.position, Quaternion.identity);
+
+        }
+        else
+        {
+            Instantiate(person, rightSpawn.transform.position, Quaternion.identity);
+        }
+    }
+}
